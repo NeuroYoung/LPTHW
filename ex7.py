9bc00f08afe62e32bd8f01130eb78a915fb3eab6
NeuroYoung <runminlyck@gmail.com>
2016-12-28 16:29:56 +0800

    ex8

--- ex7.py
+++ ex7.py
@@ -1,20 +1,20 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
+print "Mary had a little lamb." # print a string
+print "Its fleece was white as %s." % 'snow' # print a string with a formatter in it, %s stands for "snow"
 print "." * 10 # what'd that do? could give ten repicate of "."
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "C" # assign a character to a variable
+end2 = "h" # assign a character to a variable
+end3 = "e" # assign a character to a variable
+end4 = "e" # assign a character to a variable
+end5 = "s" # assign a character to a variable
+end6 = "e" # assign a character to a variable
+end7 = "B" # assign a character to a variable
+end8 = "u" # assign a character to a variable
+end9 = "r" # assign a character to a variable
+end10 = "g" # assign a character to a variable
+end11 = "e" # assign a character to a variable
+end12 = "r" # assign a character to a variable
 
 # watch that comma at the end. try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6, # comma could connect the command line with the next
-print end7 + end8 + end9 + end10 + end11 + end12
+print end7 + end8 + end9 + end10 + end11 + end12 # print in two lines because over 80 characters is a bad format in Python
