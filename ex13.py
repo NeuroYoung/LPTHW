2b2409d916286b8910cc8778f2999dd442451827
NeuroYoung <runminlyck@gmail.com>
2016-12-28 21:26:13 +0800

    ex13

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+four = raw_input("do what?")
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your fourth variable is:", four
