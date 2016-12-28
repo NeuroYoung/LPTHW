76c796c1b8203b6b13ee460de41044351e2e3e49
NeuroYoung <runminlyck@gmail.com>
2016-12-29 01:14:03 +0800

    ex15

--- ex15.py
+++ ex15.py
@@ -1,15 +1,15 @@
-from sys import argv
+from sys import argv # import argv from sys(module/library) into our enviroment
 
-script, filename = argv
+script, filename = argv # unpack the argv and assign it to 2 variables
 
-txt = open(filename)
+txt = open(filename) # open the file you designated to filename
 
-print "Here's your file %r:" % filename
-print txt.read()
+print "Here's your file %r:" % filename # print the file's name you wanna to open
+print txt.read() # print the contents in the file
 
-print "Type the filename again:"
-file_again = raw_input(">")
+print "Type the filename again:" # suggest to input another file's name
+file_again = raw_input(">") # input the name, assign it to the variable file_again
 
-txt_again = open(file_again)
+txt_again = open(file_again) # open the file
 
-print txt_again.read()
+print txt_again.read() # read it
