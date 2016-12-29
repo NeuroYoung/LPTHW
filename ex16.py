7d4ab246033ed0f9c16b5b160efa5438da0a6a8a
NeuroYoung <runminlyck@gmail.com>
2016-12-29 08:42:42 +0800

    ex16

--- ex16.py
+++ ex16.py
@@ -22,12 +22,15 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+
+#target.write(line1)
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
+
+target.write("%r\t\t  %r\n%r\n" % (line1 , line2, line3))
 
 print "And finally, we close it."
 target.close()
