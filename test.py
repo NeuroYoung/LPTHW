5fd666a4292b3cc3ea9af283f3f5dfff5211605d
NeuroYoung <runminlyck@gmail.com>
2017-01-01 23:09:28 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,38 @@
+# "There is no failure, only trying."
+cd # change directory
+print # print the contents within qoute on the screen
++ # Addition
+- # subtraction
+* # multiply
+/ # divide, if the num is not a float, then the operation will drop all the fractions
+% # means te remainder when doing a division operation
+< # less-than
+> # greater-than
+<- # less-than-equal
+>- # greater-than-equal
+== # equal to
+%r %s %d %f # formatters/ string formatting operations, replace the value behind the % within a string. r means raw data, f means floating, d means int decimal, s means string
+True/False # boolean values
+string * Num # replication of that string in Num times
+\n # escape sequence 换码顺序, "linefeed"
+""" # print all within """
+\t # Horizontal Tab(TAB)
+\' \" \\ # single-quote double-quote and backslash
+\r # carriage return 回车
+raw_input() # input from user into the script, could change the prompt
+sys # a library/module, we could import many functions from it
+argv # argument variable, could unpack the arguments user input and assign to other arguments
+open # open a file and assign it to some variable
+file.read # read the file, usually combine with print
+file.write # write contents into the file
+os.path # the library consisting of fuctions related to the OS paths
+len # length of the string
+exists # to check whether the path is exists, return a boolean value
+def # define a function, def function_name(arguemnts):
+*args # save all the arguments user give to in the variable args
+file.seek() # move to the certain byte in the file
+file.readline() # move to the certain \n in the file
+return # assign the result of the function to some variable
+.split
+sorted()
+words.pop
